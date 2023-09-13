e22c6059a034de532568c0ffbc15d433b56a8a87
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-09-13 10:01:58 -0500

    graficos chartjs

--- src/app/views/reports/reports.component.ts
+++ src/app/views/reports/reports.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Chart, registerables } from 'chart.js';
+import { Order } from 'src/app/models/order';
+import { OrderService } from '../../services/orders/orders.service';
+import { Chart, registerables, Colors } from 'chart.js';
 Chart.register(...registerables);
+Chart.register(Colors);
 
 @Component({
   selector: 'app-reports',
@@ -10,55 +13,50 @@ Chart.register(...registerables);
 })
 export class ReportsComponent implements OnInit {
 
-  public barChartOptions: any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-
-  public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
-
-  public barChartData: any[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
-  ];
-
-
   chart : any;
+  orders: Order[] = [];
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe((queryParams) => {
-
-      const tipoventas = queryParams['tipoventas'];
-      console.log('tipo:', tipoventas);
+  constructor(private route: ActivatedRoute, private orderService: OrderService) {}
+  
+  getOrders() {
+    this.orderService.get('list').subscribe((orders) => {
+      this.orders = orders.filter(
+        (order) => order.order_state != 3 && order.order_state != 4
+      );
     });
+  }
+  
+  generarChart() {
+    // Función para generar colores hexadecimales aleatorios
+    function getRandomHexColor(alpha = 0.3) {
+      // Generar valores aleatorios para R, G y B en el rango [0, 255]
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      
+      const rgbaColor = `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    
+      return rgbaColor;
+    }
+    
+    // Datos para el gráfico (puedes actualizar estos datos según tus necesidades)
+    const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+    const data = [12, 19, 3, 5, 2, 3];
 
+    // Genera colores hexadecimales aleatorios para backgroundColor y borderColor
+    const backgroundColor = labels.map(() => getRandomHexColor());
+    const borderColor = backgroundColor; // El borderColor es el mismo que backgroundColor
+
+    // Configuración del gráfico
     var myChart = new Chart("myChart", {
       type: 'bar',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: labels,
           datasets: [{
               label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              data: data,
+              backgroundColor: backgroundColor,
+              borderColor: borderColor,
               borderWidth: 1
           }]
       },
@@ -69,7 +67,18 @@ export class ReportsComponent implements OnInit {
               }
           }
       }
-  });
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((queryParams) => {
+
+      const tipoventas = queryParams['tipoventas'];
+      console.log('tipo:', tipoventas);
+    });
+
+    this.generarChart();
+  
   }
 
 }
