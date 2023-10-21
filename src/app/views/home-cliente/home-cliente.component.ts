3064313cedd598966e0aff8682dc0b791ece906c
Cristian B <ccbonillar@uqvirtual.edu.co>
2023-10-21 15:17:46 -0500

    cambios para configuracion del home de clientes

--- src/app/views/home-cliente/home-cliente.component.ts
+++ src/app/views/home-cliente/home-cliente.component.ts
@@ -9,6 +9,12 @@ import { sectionService } from 'src/app/services/sectionService/section.service'
 import { Section } from 'src/app/models/section';
 import {FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+const LOAD_COMPONENT = Object.freeze({
+  load: 1,
+  configSecciones: 2,
+  previsualizacionHome: 3,
+});
+
 @Component({
   selector: 'app-home-cliente',
   templateUrl: './home-cliente.component.html',
@@ -16,41 +22,76 @@ import {FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class HomeClienteComponent {
   seccionSelected: Section= new Section();
-  seccionFormGroup!: FormGroup;
+  seccionFormGroup: FormGroup;
   cliente: User = new User();
   sectionList: Section [] = [];
+  isPrevisualizarOpen = false;
+  checked = false;
+  estadoComponente: number = LOAD_COMPONENT.configSecciones;
 
   backgroundImages: string[] = [
   ];
 
-  constructor(private sectionService: sectionService, private formBuilder: FormBuilder) {}
+  
+  get loadingComponent() {
+    return this.estadoComponente === LOAD_COMPONENT.load;
+  }
+  get configComponent() {
+    return this.estadoComponente === LOAD_COMPONENT.configSecciones;
+  }
+  get viewHomeClientes() {
+    return this.estadoComponente === LOAD_COMPONENT.previsualizacionHome;
+  }
+  
+  constructor(private sectionService: sectionService, private formBuilder: FormBuilder) {
+    this.seccionFormGroup = this.formBuilder.group({
+      titulo_seccion: [null],
+      descripcion: [null],
+      posicion: [null],
+      image: [null],
+      opacity: [null],
+      visible: [null],
+    });
+  }
 
   getInfoSecciones() {
     this.sectionService.get('list').subscribe((res) => {
       console.log('get sections '+JSON.stringify(res));
-      this.seccionSelected = res[0];
       this.sectionList = res;
       this.sectionList.sort((a, b) => a.posicion - b.posicion);
+      this.seccionSelected = res[0];
+      this.tabs = []; 
       this.sectionList.forEach((section: Section) => {
         console.log('IMAGE TO ADD '+section.image);
         this.backgroundImages.push('url('+section.image+')');
+        this.tabs.push(''+section.posicion);
       });
       console.log('fianl background image '+JSON.stringify(this.backgroundImages));
       console.log('get sections 1 '+JSON.stringify(this.seccionSelected) );
+      
+      this.createForm();
     });
   }
 
   ngOnInit(): void {
+    this.isPrevisualizarOpen = false;
     this.getInfoSecciones();
-    this.createForm()
   }
   createForm() {
-    this.seccionFormGroup = this.formBuilder.group({
-      order_date: [null, [Validators.required]],
-      value: [null, [Validators.required]],
-      discount: [null, [Validators.required]],
-      state: [null, [Validators.required]],
-      products: [null, [Validators.required]],
+    //this.seccionFormGroup = this.formBuilder.group({
+    //  titulo_seccion: [null],
+    //  //posicion: [null],
+    //  //descripcion: [null],
+    //  //seccion_id: [null],
+    //});
+    this.seccionFormGroup.patchValue({
+      titulo_seccion: this.seccionSelected.home_tittle,
+      descripcion: this.seccionSelected.description,
+      posicion: this.seccionSelected.posicion,
+      image: this.seccionSelected.image,
+      opacity: this.seccionSelected.opacity,
+      visible: this.seccionSelected.visible,
+      // Otros campos del formulario
     });
   }
   
@@ -73,8 +114,79 @@ export class HomeClienteComponent {
     });
   }
 
-  tabs = ['1', '2', '3'];
+  tabs = ['1']; //'1', '2', '3'
   selected = new FormControl(0);
+
+  cargarImagen(): void {
+    console.log('URL en cargar IMAGEN : '+this.seccionFormGroup.controls['image'].value);
+    this.seccionSelected.image = this.seccionFormGroup.controls['image'].value;
+  }
+
+  reloadListaSecciones(cargar: Boolean): void {
+    this.sectionService.get('list').subscribe((res) => {
+      this.sectionList = res;
+      this.sectionList.sort((a, b) => a.posicion - b.posicion);
+      this.seccionSelected = res[this.selected.value ?? 0];
+      this.tabs = []; 
+      this.backgroundImages = [];
+      this.sectionList.forEach((section: Section) => {
+        console.log('IMAGE TO ADD '+section.image);
+        this.backgroundImages.push('url('+section.image+')');
+        this.tabs.push(''+section.posicion);
+      });
+    });
+    if(cargar){
+      this.cargarImagen();
+    }
+  }
+
+  guardarSeccion(): void {
+    var seccionUpdate = new Section();
+    seccionUpdate = this.sectionList[this.selected.value ?? 0];
+    console.log('id de la seccion update : '+this.seccionSelected.section_id);
+    seccionUpdate.home_tittle = this.seccionFormGroup.controls['titulo_seccion'].value;
+    seccionUpdate.description = this.seccionFormGroup.controls['descripcion'].value;
+    seccionUpdate.posicion = this.seccionFormGroup.controls['posicion'].value;
+    seccionUpdate.image = this.seccionFormGroup.controls['image'].value;
+    seccionUpdate.opacity = this.seccionFormGroup.controls['opacity'].value;
+    seccionUpdate.visible = this.seccionFormGroup.controls['visible'].value;
+    
+    console.log('SEc update : '+JSON.stringify(seccionUpdate));
+
+    this.sectionService.put(`update/${this.seccionSelected.section_id}`, seccionUpdate).subscribe((res) => {
+      console.log('RESULT UPDAte section '+JSON.stringify(res));
+      this.reloadListaSecciones(true);
+    });
+    
+  }
+  salirprevisualizarHome(): void {
+    this.estadoComponente = LOAD_COMPONENT.configSecciones;
+    console.log('entro previsualizar : '+this.isPrevisualizarOpen);
+    this.isPrevisualizarOpen = !this.isPrevisualizarOpen;
+  }
+
+  previsualizarHome(): void {
+    this.reloadListaSecciones(false);
+    this.estadoComponente = LOAD_COMPONENT.previsualizacionHome;
+    console.log('entro previsualizar : '+this.isPrevisualizarOpen);
+    this.isPrevisualizarOpen = !this.isPrevisualizarOpen;
+  }
+  onTabSelectionChange(selectedIndex: number) {
+    this.selected.setValue(selectedIndex);
+    this.seccionSelected = this.sectionList[selectedIndex];
+  
+    // Actualiza el valor del formulario con los datos de la sección seleccionada
+    //this.seccionFormGroup.setValue(this.seccionSelected);
+
+    this.seccionFormGroup.patchValue({
+      titulo_seccion: this.seccionSelected.home_tittle,
+      descripcion: this.seccionSelected.description,
+      posicion: this.seccionSelected.posicion,
+      image: this.seccionSelected.image,
+      opacity: this.seccionSelected.opacity,
+      visible: this.seccionSelected.visible,
+    });
+  }
 
   addTab() {
     this.tabs.push('New');
